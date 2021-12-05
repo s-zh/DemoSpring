926c0a19c2d75e0176afbd18207dae5b82c6bdc4
szh <1220298025@qq.com>
2021-12-05 15:20:58 +0800

    0.1ver

--- src/main/java/springframework/BeanFactory.java
+++ src/main/java/springframework/BeanFactory.java
@@ -6,19 +6,19 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * @Author:吉口吉
  * @Date:Created in 14:17 2021/12/5
- * @Description: 容器，用HashMap存放BeanDefinition
+ * @Description: 容器，用HashMap托管bean
  */
 
 public class BeanFactory {
 
-    private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
+    private Map<String, Object> beanMap = new ConcurrentHashMap<>();
 
     public Object getBean(String name) {//bean的获取
-        return beanDefinitionMap.get(name).getBean();
+        return beanMap.get(name);
     }
 
-    public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {//存放bean的定义信息
-        beanDefinitionMap.put(name, beanDefinition);
+    public void registerBean(String name, Object bean) {//托管bean
+        beanMap.put(name, bean);
     }
 
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,14 +1,12 @@
 import pojo.User;
-import springframework.BeanDefinition;
 import springframework.BeanFactory;
 
 public class Test {
     public static void main(String[] args){
-        //1.初始化BeanFactory
+        //1.初始化容器BeanFactory
         BeanFactory beanFactory=new BeanFactory();
-        //2.注入bean
-        BeanDefinition beanDefinition=new BeanDefinition(new User());
-        beanFactory.registerBeanDefinition("user",beanDefinition);
+        //2.托管bean
+        beanFactory.registerBean("user",new User());
         //3.获取bean
         User u=(User)beanFactory.getBean("user");
         u.UserSpeak();
